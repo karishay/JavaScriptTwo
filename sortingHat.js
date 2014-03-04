053599f365e9f365cef1d58b39a6d582f8eb15b5
Hackbright Student <user@mini-002.(none)>
2014-03-03 17:50:31 -0800

    Need to fix capacity

--- sortingHat.js
+++ sortingHat.js
@@ -22,31 +22,62 @@ console.log(houses);
 var listOfHouses = Object.keys(houses);
 console.log(listOfHouses);
 
-
-
-function sortStudents() {
-    var studentName = document.getElementById("student_name");
-    var randNum = Math.random() * 5;
+function generateRandomHouse(){
+ var randNum = Math.random() * 5;
     //this will correlate to an index in our list of keys (houses)   
     var randIndex = Math.floor(randNum);
     //randomly pick a house 
     var randHouse = listOfHouses[randIndex];
-    console.log(randHouse);
-    //define the capacity of the house
-    var capacity = studentCount/listOfHouses.length;
-    console.log(capacity);
-    //check if the capacity of the house if full
-    if (houses[randHouse].length < capacity){
-        //add the student to the house in HTML
+    return randHouse;
+}
+
+function sortStudents() {
+    var originalStudentCount = studentCount;
+    console.log("this is the original student count" + originalStudentCount);
+    var studentName = document.getElementById("student_name");
+    var capacity = originalStudentCount/listOfHouses.length;
+    console.log("This is the capacity: " + capacity);
+
+    if (originalStudentCount > 1){
+        var randHouse = generateRandomHouse();
+        console.log("This is the length of the random house" + randHouse.length);
+        while (randHouse.length == capacity) {
+            randHouse = generateRandomHoue();
+        }
+        //add the student to the house in HTML      
         var studentNode = document.getElementById(randHouse);
         studentNode.innerHTML += "<br>" + studentName.value;
         //add the student to the dictionary list for that house
         houses[randHouse].push(studentName.value);
         //decrease the student count by one
         studentCount -= 1;
-    } else {
-        sortStudents();
     }
+
+//     var studentName = document.getElementById("student_name");
+//     var randHouse = generateRandomHouse();
+//     //define the capacity of the house
+//     var capacity = studentCount/listOfHouses.length;
+//     console.log(capacity);
+//     //check if the capacity of the house is full
+//     // set a while loop to run until the studentCount is 0
+//     if (studentCount !== 0){
+//         // if the randomly generated house is not at capacity,
+//         // add the student to the house in HTML and to the value in the house
+//         // dictionary
+//         // Then break the loop.
+//         if (houses[randHouse].length < capacity){
+//             //add the student to the house in HTML
+//             var studentNode = document.getElementById(randHouse);
+//             studentNode.innerHTML += "<br>" + studentName.value;
+//             //add the student to the dictionary list for that house
+//             houses[randHouse].push(studentName.value);
+//             //decrease the student count by one
+//             studentCount -= 1;
+//         } else if (houses[randHouse].length === capacity){
+//             randHouse = generateRandomHouse();
+//         }
+//     }
+// }
 }
 
 sortButton.addEventListener("click", sortStudents);
