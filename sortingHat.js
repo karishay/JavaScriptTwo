8cd2a8e3e7e3fd8ee5aa59e603e146df89a5accf
Hackbright Student <user@mini-002.(none)>
2014-03-04 11:27:08 -0800

    Finished sorting students properly. Can go back and do bonus if desired.

--- sortingHat.js
+++ sortingHat.js
@@ -1,5 +1,6 @@
 var studentCount = 0;
 var capacity = 0;
+var totalStudents = 0;
 
 var button = document.getElementById("set_student_count");
 
@@ -9,10 +10,13 @@ function countEvent() {
     // retrieving the value of form
     var inputVal = document.getElementById("student_count").value;
     // updating studentCount by inputVal. Passed through parseInt so that it's a number!
+    totalStudents += Number(inputVal);
     studentCount += Number(inputVal);
-    capacity = Math.floor(StudentCount/5) + 1;
+    console.log(studentCount);
+    capacity = Math.floor(totalStudents/5);
+    console.log(capacity);
     // Updating the HTML (the div id of count) so the current count displays. 
-    countVar.innerHTML = Number(studentCount);
+    countVar.innerHTML = "The current amount of students that need to be sorted: " + Number(studentCount);
     // resetting the value of the form to 0
     document.getElementById("student_count").value = 0;
 }
@@ -34,20 +38,21 @@ function generateRandomHouse(){
     return randHouse;
 }
 
-
-//student count is broken. I'm not even sure how its broken right now...
-//student count is changing, has to do with scoping and globals.
+//Now, for reals it works. 
 function sortStudents() {
     console.log("this is the new student count" + studentCount);
     var studentName = document.getElementById("student_name");
 
     console.log("This is the capacity: " + capacity);
 
-    if (studentCount !== 1){
+    if (studentCount !== 0){
         var randHouse = generateRandomHouse();
-        console.log("This is the length of the random house" + randHouse.length);
-        while (randHouse.length == capacity) {
-            randHouse = generateRandomHoue();
+        
+        console.log("The house we selected is: " + randHouse);
+        while (houses[randHouse].length == capacity) {
+            console.log("The " + randHouse + " was full");
+            randHouse = generateRandomHouse();
+            console.log("The new random house is: " + randHouse);
         }
         //add the student to the house in HTML      
         var studentNode = document.getElementById(randHouse);
@@ -56,33 +61,11 @@ function sortStudents() {
         houses[randHouse].push(studentName.value);
         //decrease the student count by one
         studentCount -= 1;
+        console.log("The number of students in the house should be: " + houses[randHouse].length);
+    } else {
+        var sortNode = document.getElementById("sort");
+        sortNode.innerHTML += "There are no more students to sort. Waaa Waaaah.";
     }
-
-//     var studentName = document.getElementById("student_name");
-//     var randHouse = generateRandomHouse();
-//     //define the capacity of the house
-//     var capacity = studentCount/listOfHouses.length;
-//     console.log(capacity);
-//     //check if the capacity of the house is full
-//     // set a while loop to run until the studentCount is 0
-//     if (studentCount !== 0){
-//         // if the randomly generated house is not at capacity,
-//         // add the student to the house in HTML and to the value in the house
-//         // dictionary
-//         // Then break the loop.
-//         if (houses[randHouse].length < capacity){
-//             //add the student to the house in HTML
-//             var studentNode = document.getElementById(randHouse);
-//             studentNode.innerHTML += "<br>" + studentName.value;
-//             //add the student to the dictionary list for that house
-//             houses[randHouse].push(studentName.value);
-//             //decrease the student count by one
-//             studentCount -= 1;
-//         } else if (houses[randHouse].length === capacity){
-//             randHouse = generateRandomHouse();
-//         }
-//     }
-// }
 }
 
 sortButton.addEventListener("click", sortStudents);
